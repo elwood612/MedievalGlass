3552aed4dc8eba9f82d726d95bdbaf122e9b63ea
elwood612 <30651423+elwood612@users.noreply.github.com>
2026-06-03 11:55:51 +0200

    Backportable feature set - Squash

---
+++ forge/src/main/java/com/github/elwood612/medievalglass/MedievalGlass.java
@@ -0,0 +1,12 @@
+package com.github.elwood612.medievalglass;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class MedievalGlass
+{
+
+    public MedievalGlass() {
+        CommonClass.init();
+    }
+}
